6eaf6c23d31f132fe3be95c3a5ebdd48c756edea
lbot <find.creativity@leematos.com>
2012-10-11 15:15:02 -0400

    Convert to Flask

--- img_to_json.py
+++ img_to_json.py
@@ -1,27 +1,45 @@
+from flask import Flask, request,jsonify
 import json
 import urllib2
 import cStringIO
 import base64
-import webapp2
+
+"""
+Taken from:  https://gist.github.com/1094140
+"""
+
+from functools import wraps
+from flask import request, current_app
 
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
+def jsonp(func):
+    """Wraps JSONified output for JSONP requests."""
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            data = str(func(*args, **kwargs).data)
+            content = str(callback) + '(' + data + ')'
+            mimetype = 'application/javascript'
+            return current_app.response_class(content, mimetype=mimetype)
+        else:
+            return func(*args, **kwargs)
+    return decorated_function
 
-        f = urllib2.urlopen(urllib2.unquote('http://static.guim.co.uk/sys-images/%s') % self.request.get('img'))
-        im = cStringIO.StringIO(f.read())
+
+app = Flask(__name__)
+
+@app.route("/")
+@jsonp
+def hello():
+        f = urllib2.urlopen(urllib2.unquote('http://static.guim.co.uk/sys-images/%s') % request.args.get('img'))
+	im = cStringIO.StringIO(f.read())
         return_image = base64.b64encode(im.getvalue())
         type_prefix = "data:image/jpg;base64,"
         data = {
                             "data": type_prefix + return_image
                         }
+    	return jsonify(data)
 
-        self.response.headers['Content-Type'] = 'application/json'
-        if 'callback' in self.request.arguments():
-                data = '%s(%s)' % (self.request.get('callback'), json.dumps(data))
-                self.response.out.write(data)
-        else:
-                self.response.out.write(json.dumps(data))
-
-
-app = webapp2.WSGIApplication([('/img_to_json', MainPage)], debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
